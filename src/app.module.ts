76010b7cd159f112f63d45e06276630c328c3eff
mohdzaid <mohdzaid@gmail.com>
2022-09-16 15:14:52 +0530

    graphql and nestjs crud

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver } from '@nestjs/apollo/dist/drivers';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+    }),
+    MongooseModule.forRoot('mongodb://localhost/nestgraphql'),
+    ItemsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
